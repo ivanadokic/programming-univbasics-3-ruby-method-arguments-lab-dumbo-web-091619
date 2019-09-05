2399027a9f525db9db119d01156914dad8a954be
dependable-helper-6910 <ivana_pile@yahoo.com>
2019-09-05 20:51:11 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,6 +6,7 @@ def introduction_with_language (name, language)
   
   def introduction_with_language_optional (name, language="Ruby" )
     uts "Hi, my name is #{name} and I am learning to program in #{language}."
+  end
 
 
 # Write your code here. If you get stuck, at the very bottom of this lab we've included
